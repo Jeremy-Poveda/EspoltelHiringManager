85a5cba35e0222aa302529487c1d149ad3e10e5e
Jeremy-Poveda <jerypovedag@gmail.com>
2025-02-11 10:25:32 -0500

    Spring Boot base + Maven, and Hello world test

---
+++ src/main/java/espoltel/hiringmanager/HolaMundo.java
@@ -0,0 +1,12 @@
+package espoltel.hiringmanager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundo {
+	@GetMapping("/")
+	public String hola() {
+		return "Hola mundo";
+	}
+}
---
+++ src/test/java/espoltel/hiringmanager/EspoltelHiringManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package espoltel.hiringmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EspoltelHiringManagerApplication.class)
+class EspoltelHiringManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Prueba básica para verificar que el contexto de Spring se carga correctamente
+    }
+}
